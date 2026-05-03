1eaeaac268f9f469884720130c773af7fc924072
Tamoon <sksqlrlek@gmail.com>
2026-05-03 23:01:40 +0900

    feat: axiosInstance 설정 (싱글톤 패턴, JWT 인터셉터)

---
+++ src/api/axiosInstance.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:8080',
+  timeout: 10000,
+})
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('accessToken')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('accessToken')
+      window.location.href = '/login'
+    }
+    return Promise.reject(error)
+  }
+)
+
+export default axiosInstance
