77f4541ab494e88df8c830922291070a77e061a2
Tamoon <sksqlrlek@gmail.com>
2026-05-03 23:02:13 +0900

    feat: 라우터 설정 (BrowserRouter, AuthProvider 연결)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
+import { ROUTES } from './constants/routes'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import SignupPage from './pages/SignupPage'
+import StockDetailPage from './pages/StockDetailPage'
+import WatchlistPage from './pages/WatchlistPage'
+import MyPage from './pages/MyPage'
+
 function App() {
   return (
-    <div className="text-3xl font-bold text-blue-500">
-      StockMentor
-    </div>
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path={ROUTES.HOME} element={<HomePage />} />
+          <Route path={ROUTES.LOGIN} element={<LoginPage />} />
+          <Route path={ROUTES.SIGNUP} element={<SignupPage />} />
+          <Route path={ROUTES.STOCK_DETAIL} element={<StockDetailPage />} />
+          <Route path={ROUTES.WATCHLIST} element={<WatchlistPage />} />
+          <Route path={ROUTES.MY_PAGE} element={<MyPage />} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
   )
 }
 
