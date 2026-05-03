c3b27ff38daab4784b078cdd89d91fd92d46022a
Tamoon <sksqlrlek@gmail.com>
2026-05-03 23:02:04 +0900

    feat: 라우트 경로 상수 정의

---
+++ src/constants/routes.js
@@ -0,0 +1,10 @@
+export const ROUTES = {
+  HOME: '/',
+  LOGIN: '/login',
+  SIGNUP: '/signup',
+  STOCK_DETAIL: '/stock/:stockCode',
+  WATCHLIST: '/watchlist',
+  ANALYSIS: '/analysis',
+  ANALYSIS_DETAIL: '/analysis/:id',
+  MY_PAGE: '/mypage',
+}
