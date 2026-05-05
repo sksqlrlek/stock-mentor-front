e5084563739864f17d4fcef63dfb2588ae2bd2f6
Tamoon <sksqlrlek@gmail.com>
2026-05-05 12:21:24 +0900

    feat: RiskTypeModal 연결 (riskType 없는 유저 자동 감지)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
+import { useAuth } from './context/AuthContext'
 import Layout from './components/layout/Layout'
+import RiskTypeModal from './components/common/RiskTypeModal'
 import { ROUTES } from './constants/routes'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
@@ -9,18 +11,32 @@ import StockDetailPage from './pages/StockDetailPage'
 import WatchlistPage from './pages/WatchlistPage'
 import MyPage from './pages/MyPage'
 
+function AppContent() {
+  const { user } = useAuth()
+  const showRiskTypeModal = user !== null && user.riskType === null
+
+  return (
+    <>
+      {showRiskTypeModal && (
+        <RiskTypeModal onClose={() => {}} />
+      )}
+      <Routes>
+        <Route path={ROUTES.HOME} element={<Layout><HomePage /></Layout>} />
+        <Route path={ROUTES.LOGIN} element={<LoginPage />} />
+        <Route path={ROUTES.SIGNUP} element={<SignupPage />} />
+        <Route path={ROUTES.STOCK_DETAIL} element={<Layout><StockDetailPage /></Layout>} />
+        <Route path={ROUTES.WATCHLIST} element={<Layout><WatchlistPage /></Layout>} />
+        <Route path={ROUTES.MY_PAGE} element={<Layout><MyPage /></Layout>} />
+      </Routes>
+    </>
+  )
+}
+
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          <Route path={ROUTES.HOME} element={<Layout><HomePage /></Layout>} />
-          <Route path={ROUTES.LOGIN} element={<LoginPage />} />
-          <Route path={ROUTES.SIGNUP} element={<SignupPage />} />
-          <Route path={ROUTES.STOCK_DETAIL} element={<Layout><StockDetailPage /></Layout>} />
-          <Route path={ROUTES.WATCHLIST} element={<Layout><WatchlistPage /></Layout>} />
-          <Route path={ROUTES.MY_PAGE} element={<Layout><MyPage /></Layout>} />
-        </Routes>
+        <AppContent />
       </AuthProvider>
     </BrowserRouter>
   )
